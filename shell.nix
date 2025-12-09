34cbf14e2e8670a522447aa39ca2dd11d4407f9b
theoahfeldt <34159999+theoahfeldt@users.noreply.github.com>
2025-12-09 18:43:20 +0100

    Set cookie

--- shell.nix
+++ shell.nix
@@ -5,4 +5,5 @@ with import <nixpkgs> {};
       pkgs.beamMinimal28Packages.erlang
       pkgs.gleam
     ];
+    AOC_COOKIE = "53616c7465645f5f958b18a0c9af44c11954c08ae367df57c5635f7241562b46ec5e517b8792767d7e49f898f042532da56600f8b38e8c45c3b88ec9e867ad1a";
   }
